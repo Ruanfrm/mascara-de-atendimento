ff927eae74b588fd956f745f1f499f563fd8e710
Ruan Freire <91690613+Ruanfrm@users.noreply.github.com>
2024-10-05 15:27:30 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4 text-center">Sistema de Atendimento ao Cliente</h1>
+      <h1 className="text-3xl font-bold mb-4 text-center bg-black	">Sistema de Atendimento ao Cliente</h1>
       <Formulario />
       <Footer/>
     </div>
